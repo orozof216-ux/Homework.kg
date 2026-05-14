e0ee02f295594dd304793b7c7f1e7d594dafe4ad
orozof216-ux <orozof216@gmail.com>
2026-05-14 18:08:57 +0600

    homework6

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.auth_login_view, name='login'),
